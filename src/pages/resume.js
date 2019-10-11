b8a4c7758d3a6f42ae8cac91c1eab3c40bd334c5
ElyriaG <ElyriaG@users.noreply.github.com>
2019-10-10 21:58:04 -0500

    Resume link opens up resume img

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, { useState } from "react";
+import ResumeImage from "./../images/Resume.png";
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import LeftDrawer from "./../components/drawer";
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { Document } from 'react-pdf';
 
 
 const useStyles = makeStyles(theme => ({
     root: {
         display: 'flex',
     },
+    textColor: {
+        color: "white",
+    },
+    resumeDimensions: {
+        width: 700,
+        height: 900,
+        marginTop: 30,
+    },
+    divDimensions: {
+        display: "flex",
+        justifyContent: "center",
+        flexWrap: 'wrap'
+    }
+
 }));
 
 
-function Resume() {
+export default function Resume(props) {
+
+    const open = useState(false);
     const classes = useStyles();
+
+
+
     return (
-        <div className={classes.root}>
+        <div>
             <CssBaseline />
             <LeftDrawer />
-            <Document file="./../images/" />
+
+            <main
+                className={clsx(classes.content, {
+                    [classes.contentShift]: open,
+                })}
+            >
+
+                <div className={classes.drawerHeader} />
+                <br />
+                <h2 className={classes.textColor}>My Resume</h2>
+                <div className={classes.divDimensions}>
+                    <img className={classes.resumeDimensions} src={ResumeImage} alt="Resume" />
+                </div>
+            </main>
+
         </div>
     )
 }
 
-export default Resume;
